45aa36e4a5c6ebeccd6351ce5d65385dabba240c
Shawn Presser <shawnpresser@gmail.com>
2018-12-11 11:29:38 -0600

    Add ln.scm startup file

---
+++ ln.scm
@@ -0,0 +1,18 @@
+; racket -f as.scm
+; (asv)
+; http://localhost:8080
+
+(require mzscheme) ; promise we won't redefine mzscheme bindings
+
+(require "ac.scm") 
+(require "brackets.scm")
+(use-bracket-readtable)
+
+(aload "arc.arc")
+(aload "libs.arc") 
+
+(aload "news.arc")
+(arc-eval '(= srv-noisy* t))
+(arc-eval '(nsv))
+
+
