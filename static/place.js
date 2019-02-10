f95031b5be667cfbcb18a585f38042c16874585e
Emily Kolar <emkolardev@gmail.com>
2019-02-10 04:10:00 -0600

    click events on buttons

--- static/place.js
+++ static/place.js
@@ -1,19 +1,81 @@
 (function() {
-  function start() {
-    const source = new EventSource('/place.events');
+  const markValue = String.fromCharCode(215);
+  let placeQuery;
 
-    source.addEventListener('put', function (evt) {
-      const { data = {} } = evt;
-      const json = JSON.parse(data);
-      const { path, data: d } = json;
+  function sendRequest(url, options = {}, data) {
+    const fetchOptions = Object.assign({}, options);
 
-      const item = byId(path).firstElementChild.firstElementChild;
+    if (data) fetchOptions.body = JSON.stringify(data);
 
-      Object.keys(d.style).forEach((k) => {
-        item.style[k] = d.style[k];
-      });
+    fetch(url, fetchOptions)
+      .catch(err => console.log(err));
+  }
+
+  const makeFromQuery = (fromId) => `from=${fromId}`;
+  const makePathString = (fromStr, toId) => `/placeop?${fromStr}&to=${toId}`;
+  const url = (pathString) => {
+    const { host, protocol } = location;
+    return new URL(pathString, `${protocol}//${host}`);
+  };
+
+  function handleClick(evt) {
+    const cell = evt.target.parentNode;
+
+    if (placeQuery) {
+      const inputVal = evt.target.getAttribute('value');
+
+      if (inputVal === markValue) {
+        placeQuery = undefined;
+        evt.target.setAttribute('value', '');
+      } else {
+        const urlPath = makePathString(placeQuery, cell.id);
+        const urlObj = url(urlPath);
+        sendRequest(urlObj.href, {
+          method: 'POST',
+        });
+      }
+    } else {
+      placeQuery = makeFromQuery(cell.id);
+      evt.target.setAttribute('value', markValue);
+    }
+  }
+
+  function listenForSubmit() {
+    const buttons = document.querySelectorAll('#place tr td input');
+    buttons.forEach((b) => {
+      b.addEventListener('click', handleClick);
     });
   }
 
-  window.addEventListener('load', start);
+  const handlePutEvent = function (evt) {
+    const { data = {} } = evt;
+    const json = JSON.parse(data);
+    const { path, data: d } = json;
+
+    const item = byId(path).firstElementChild;
+
+    Object.keys(d.style).forEach((k) => {
+      item.style[k] = d.style[k];
+    });
+  };
+
+  function startEvents() {
+    const source = new EventSource('/place.events');
+
+    source.addEventListener('put', handlePutEvent);
+
+    source.addEventListener('kill', function () {
+      source.removeEventListener('put', handlePutEvent);
+    });
+  }
+
+  function init() {
+    const { pathname } = location;
+    if (pathname === '/l/place' || pathname === '/place') {
+      listenForSubmit();
+      startEvents();
+    }
+  }
+
+  window.addEventListener('load', init);
 })();
