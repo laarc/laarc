28d2fdc9008ac90186229b4c1c1c0d6902927fad
Emily Kolar <emkolardev@gmail.com>
2019-02-10 01:25:57 -0600

    update elements from sse

--- static/place.js
+++ static/place.js
@@ -1,6 +1,18 @@
 (function() {
   function start() {
-    console.log('start');
+    const source = new EventSource('/place.events');
+
+    source.addEventListener('put', function (evt) {
+      const { data = {} } = evt;
+      const json = JSON.parse(data);
+      const { path, data: d } = json;
+
+      const item = byId(path).firstElementChild.firstElementChild;
+
+      Object.keys(d.style).forEach((k) => {
+        item.style[k] = d.style[k];
+      });
+    });
   }
 
   window.addEventListener('load', start);
