7c442a1046cfb0ccec34c7b6121cf0e7d9e30db8
Emily Kolar <emkolardev@gmail.com>
2019-02-16 15:42:38 -0600

    organize code a little

--- static/place-canvas.js
+++ static/place-canvas.js
@@ -1,4 +1,5 @@
 const placeUrl = '/place.json';
+
 let colors = {};
 let board = {};
 let inf;
@@ -8,12 +9,11 @@ let ctx;
 const size = 12;
 
 // Prepare some variables for the dragging gestures logic
-var mouseIsDown = false;
-var middleOrRightIsDown = false;
-var previousMousePosition;
+let mouseIsDown = false;
+let isPanning = false;
+let previousMousePosition;
 
 function drawBoard() {
-  console.log(board);
   board.forEach((row, rowIndex) => {
     row.forEach((pixel, pixIndex) => {
       var color = colors[pixel] || { hex: 'black' };
@@ -30,6 +30,57 @@ function getBoard() {
     board = json.board;
     drawBoard();
   });
+}
+
+function windowKeydown(evt) {
+  if (evt.which === 68) {
+    isPanning = !isPanning;
+  }
+}
+
+function canvasMousedown(event) {
+  if (event.which === 1) {
+    mouseIsDown = true;
+  } else {
+    isPanning = true;
+  }
+}
+
+function windowMouseup(event) {
+  if (event.which === 1) {
+    mouseIsDown = false;
+    inf.updateChunks();
+  } else {
+    isPanning = false;
+  }
+}
+
+function windowMousemove(event) {
+  var newMousePosition = {x: event.offsetX, y: event.offsetY};
+  if (mouseIsDown && isPanning) {
+    // pan the canvas whenever dragging with the middle or right mouse button
+    var dx = previousMousePosition.x - newMousePosition.x;
+    var dy = previousMousePosition.y - newMousePosition.y;
+    // Canvas gets really messy if you do not clear it up :)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    inf.moveBy(dx, dy);
+  } else if (mouseIsDown) {
+    // draw lines when dragging with the left mouse button
+    if (previousMousePosition) {
+      ctx.beginPath();
+      ctx.moveTo(previousMousePosition.x, previousMousePosition.y);
+      ctx.lineTo(newMousePosition.x     , newMousePosition.y);
+      ctx.stroke();
+    }
+  }
+  previousMousePosition = newMousePosition;
+}
+
+function addListeners() {
+  window.addEventListener('keydown', windowKeydown);
+  canvas.addEventListener('mousedown', canvasMousedown);
+  window.addEventListener("mouseup", windowMouseup);
+  window.addEventListener("mousemove", windowMousemove);
 }
 
 function replaceTable() {
@@ -49,50 +100,7 @@ function replaceTable() {
   place.appendChild(canvas);
   ctx = canvas.getContext('2d');
   inf = infiniteCanvas.initialize(ctx);
-  window.addEventListener('keydown', function (evt) {
-    if (evt.which === 68) {
-      middleOrRightIsDown = !middleOrRightIsDown;
-    }
-  });
-  canvas.addEventListener('mousedown', function (event) {
-    // 1 is leftmousebutton, 2 is middle, 3 is left
-    if (event.which === 1) {
-      mouseIsDown = true;
-    } else {
-      middleOrRightIsDown = true;
-    }
-  });
-  window.addEventListener("mouseup", function (event) {
-    // When leftmousebutton is released, synchronise the newly
-    // drawn scribbles to the underlying buffer chunks
-    if (event.which === 1) {
-      mouseIsDown = false;
-      inf.updateChunks();
-    } else {
-      middleOrRightIsDown = false;
-    }
-  });
-  window.addEventListener("mousemove", function (event) {
-    var newMousePosition = {x: event.offsetX, y: event.offsetY};
-    if (mouseIsDown && middleOrRightIsDown) {
-      // pan the canvas whenever dragging with the middle or right mouse button
-      var dx = previousMousePosition.x - newMousePosition.x;
-      var dy = previousMousePosition.y - newMousePosition.y;
-      // Canvas gets really messy if you do not clear it up :)
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      inf.moveBy(dx, dy);
-    } else if (mouseIsDown) {
-      // draw lines when dragging with the left mosue button
-      if (previousMousePosition) {
-        ctx.beginPath();
-        ctx.moveTo(previousMousePosition.x, previousMousePosition.y);
-        ctx.lineTo(newMousePosition.x     , newMousePosition.y);
-        ctx.stroke();
-      }
-    }
-    previousMousePosition = newMousePosition;
-  });
-
+  addListeners();
 }
 
 window.addEventListener('load', getBoard);
